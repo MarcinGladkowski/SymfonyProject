8414fb35ef883cd566afc8e0126afd9a3a01f334
Martin89PL <marcingladkowski@gmail.com>
2016-11-01 18:22:02 +0100

    add theme

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace BlogBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {   
+    
+    /**
+     * @Route("/", name="homepage")
+     */
     public function indexAction()
     {
         return $this->render('BlogBundle:Default:index.html.twig');
